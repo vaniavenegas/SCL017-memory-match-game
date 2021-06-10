35ca67d171a55aefaf86d2c6f9b968bbc36e3cd1
ViviMoya <v.moyacapitan@gmail.com>
2021-06-10 11:10:50 -0400

    solucionado cambio de pagina

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
-import App from './components/App.js';
+//import App from './components/App.js';
 
-document.getElementById('root').appendChild(App()); //referencia al nodo de html
+//document.getElementById('root').appendChild(App()); //referencia al nodo de html
 // El método appendChild() inserta un nuevo nodo dentro de la estructura DOM
 
-window.addEventListener("load", inicio);
+document.getElementById("firstPage").addEventListener("click", firstPage, false);
 
-function go(){
-    document.getElementById("first-page").style.visibility="visible";
-    document.getElementById("first-page").style.display="block";
-    document.getElementById("second-page").style.visibility="hidden";
-    document.getElementById("second-page").style.display="none";
-};
-function go(){
-    document.getElementById("second-page").style.visibility="visible";
-    document.getElementById("second-page").style.display="block";
-    document.getElementById("first-page").style.visibility="hidden";
-    document.getElementById("second-page").style.display="none";
+
+function firstPage() {
+ document.getElementById("firstPage").style.display="none";
+ document.getElementById("secondPage").style.display="block";
 }
