4803ed1c5ce2f7d26df00e1a495ac87ff512c87e
ViviMoya <v.moyacapitan@gmail.com>
2021-06-10 10:04:05 -0400

    intento js 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { transformFileSync } from '@babel/core';
 import App from './components/App.js';
 
 document.getElementById('root').appendChild(App()); //referencia al nodo de html
 // El método appendChild() inserta un nuevo nodo dentro de la estructura DOM
 
+window.addEventListener("load", inicio);
+
+function go(){
+    document.getElementById("first-page").style.visibility="visible";
+    document.getElementById("first-page").style.display="block";
+    document.getElementById("second-page").style.visibility="hidden";
+    document.getElementById("second-page").style.display="none";
+};
+function go(){
+    document.getElementById("second-page").style.visibility="visible";
+    document.getElementById("second-page").style.display="block";
+    document.getElementById("first-page").style.visibility="hidden";
+    document.getElementById("second-page").style.display="none";
+}
