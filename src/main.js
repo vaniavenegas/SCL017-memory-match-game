4ab9a1f0c48918cc47c875430925f63edb79514c
vaniavenegas <venegas.venegas>
2021-06-21 16:02:50 -0400

    commit secondPage

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 import App from './components/App.js';
 
-document.getElementById('root').appendChild(App());
+document.getElementById('root').appendChild(App()); //referencia al nodo de html
+// El método appendChild() inserta un nuevo nodo dentro de la estructura DOM
+
+window.addEventListener("load", inicio);
+
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
